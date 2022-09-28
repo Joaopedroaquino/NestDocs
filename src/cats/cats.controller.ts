127aac2675a2d60344420d12194444de102782ab
joaopedroaquino <aqerhack@gmail.com>
2022-09-28 10:34:47 -0300

    first initial crud

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,20 +1,37 @@
-import { Controller, Get, Header, HttpCode, Post, Redirect, Req } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Header, HttpCode, Param, Post, Put, Redirect, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { CreateCatDto } from './create-cat-dto';
+import { UpdateCatDto } from './update-cat-dto';
 
 
 @Controller('cats')
 export class CatsController {
     @Get()
-    findAll(@Req() request: Request): string{
+    findAll(@Req() request: Request): string {
         return 'Busca todos os gatos';
     }
 
     @Post()
-    // @Header('Cache-Control', 'none')
-    // @HttpCode(204)
-    create(): string {
+    async create(@Body() CreateCatDto: CreateCatDto) {
         return 'Criar novo gato';
     }
-  
+
+    @Get(':id')
+    findOne(@Param() params): string {
+        console.log(params.id);
+        return `Retorna #${params.id} gato`;
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto){
+        return `Atualiza o #${id} gato`;
+    
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id:string){
+        return `Deleta o #${id} gato`;
+    }
+
 
 }
