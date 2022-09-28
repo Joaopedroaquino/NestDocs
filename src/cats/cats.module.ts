a4bb103b04e6b869d9fd2c3809eff7ad90aa9d8e
joaopedroaquino <aqerhack@gmail.com>
2022-09-28 11:21:05 -0300

    first initial //

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -2,7 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { CatsController } from './cats.controller';
 import { CatsService } from './cats.service';
 
-@Global()
+//@Global()
 @Module({
     controllers: [CatsController],
     providers: [CatsService],
